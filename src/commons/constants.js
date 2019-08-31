35e61c31ea04ac325bf18a47322546f40db90483
Eric Fei <guanwha@gmail.com>
2019-08-31 18:40:36 +0800

    Implemented left part layout of Login page
    1. add all dog & cat images into static folder
       - add /src/commons/constants.js to make json to include them
    
    2. add flex scss in _utilities.scss & _mixins.scss
       - flex-rsat
       - flex-rsac
       - flex-rsab
       - flex-rsbt
       - flex-csal
       - flex-csac
       - flex-csar
       - flex-csbl
       - flex-csbc
       - flex-csbr
    
    3. modify the shadow color _variables.scss (使其更明顯一些)
    
    4. modify Login.vue
       - implement title
       - implement select animal buttons
       - implement center picture
       - implement name input
       - implement send button

---
+++ src/commons/constants.js
@@ -0,0 +1,42 @@
+
+const imagePath = '/static/imgs/';
+export const animalImages = {
+  cat: {
+    0: {
+      img: `${imagePath}Cat_1.png`,
+      head: `${imagePath}CatHead_1.png`,
+    },
+    1: {
+      img: `${imagePath}Cat_2.png`,
+      head: `${imagePath}CatHead_2.png`,
+    },
+    2: {
+      img: `${imagePath}Cat_3.png`,
+      head: `${imagePath}CatHead_3.png`,
+    },
+    3: {
+      img: `${imagePath}Cat_4.png`,
+      head: '',
+    },
+  },
+  dog: {
+    0: {
+      img: `${imagePath}Dog_1.png`,
+      head: `${imagePath}DogHead_1.png`,
+    },
+    1: {
+      img: `${imagePath}Dog_2.png`,
+      head: '',
+    },
+    2: {
+      img: `${imagePath}Dog_3.png`,
+      head: `${imagePath}DogHead_3.png`,
+    },
+    3: {
+      img: `${imagePath}Dog_4.png`,
+      head: '',
+    },
+  },
+};
+
+export default animalImages;
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -9,17 +9,25 @@
           <div class="col-12 col-md-9 a1-main">
             <!-- title -->
             <div class="row justify-content-end a2-title">
-              <div class="col-12 col-md-9">Welcome</div>
+              <div class="col-12 col-md-9 flex-ccc">
+                <div class="title-main">Welcome</div>
+                <div class="title-sub mt-2">阿貓阿狗聊天室</div>
+              </div>
             </div>
             <div class="row a2-you">
               <!-- select animal type -->
               <div class="col-12 col-md-3 align-self-center a3-which">
-                <button>狗狗</button>
-                <button>貓貓</button>
+                <button :class="['btn', 'btn-animal', { 'btn-animal-active' : !selectCat }]">狗狗</button>
+                <button :class="['btn', 'btn-animal', { 'btn-animal-active' :  selectCat }]">貓貓</button>
               </div>
               <!-- naming -->
-              <div class="col-12 col-md-9 a3-name">
-                <button class="btn-enter" @click="gotoChatRoom">進入聊天</button>
+              <div class="col-12 col-md-9 a3-name flex-ccc">
+                <div class="a4-picture">
+                  <div class="shadow"/>
+                  <img :src="curAnimalImage">
+                </div>
+                <input class="form-control" type="text" placeholder="請輸入你想要的暱稱">
+                <button class="btn btn-enter" @click="gotoChatRoom">進入聊天</button>
               </div>
             </div>
           </div>
@@ -32,11 +40,28 @@
 </template>
 
 <script>
+import { animalImages } from '../commons/constants';
+
 export default {
   name: 'Login',
+  data() {
+    return {
+      selectCat: true,
+      selectIdx: 0,
+      images: animalImages,
+    };
+  },
   methods: {
     gotoChatRoom() {
       this.$router.push({ name: 'ChatRoom' });
+    },
+  },
+  computed: {
+    curAnimalImage() {
+      if (this.selectCat) {
+        return this.images.cat[this.selectIdx].img;
+      }
+      return this.images.dog[this.selectIdx].img;
     },
   },
 };
@@ -76,42 +101,118 @@ export default {
 }
 .a1-main {
   height: 80%;
-  background: green;
+  // background: green;
   @media(min-width: $limit-w-pc) {
     height: 100%;
   }
   .a2-title {
     height: 20%;
-    background: lightblue;
+    // background: lightblue;
+    color: $clr-white;
+    .title-main {
+      font-size: $f-size-5;
+      font-family: 'Fredoka One', cursive;
+      letter-spacing: 0.92px;
+      @media(min-width: $limit-w-pc) {
+        font-size: $f-size-6;
+      }
+    }
+    .title-sub {
+      font-size: $f-size-2;
+      letter-spacing: 0.48px;
+      @media(min-width: $limit-w-pc) {
+        font-size: $f-size-3;
+      }
+    }
   }
   .a2-you {
     height: 80%;
     .a3-which {
       height: 15%;
-      background: orange;
-      @include flex-rcc;
+      // background: orange;
+      @include flex-rsac;
       @media(min-width: $limit-w-pc) {
         height: 30%;
-        @include flex-ccc;
+        @include flex-csac;
       }
     }
     .a3-name {
       height: 85%;
-      background: pink;
+      // background: pink;
       @media(min-width: $limit-w-pc) {
         height: 100%;
+      }
+      .a4-picture {
+        width: 100%;
+        height: 100%;
+        position: relative;
+        img {
+          height: 70%;
+          background-size: contain;
+          background-repeat: no-repeat;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          z-index: 1;
+        }
+        .shadow {
+          width: 30%;
+          height: 7%;
+          background: $clr-shadow;
+          border-radius: 50%;
+          position: absolute;
+          bottom: 9%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          z-index: 0;
+        }
+      }
+      input {
+        width: 80%;
+        background: $clr-main-lv2;
+        color: $clr-white;
+        border-width: 0;
+      }
+      .btn-enter {
+        width: $f-size-3 * 7;
+        height: $f-size-3 * 2;
+        border-radius: $f-size-3 * 1;
+        color: $clr-main-lv1;
+        font-weight: bold;
+        font-size: $f-size-3;
+        line-height: $f-size-3 * 2;
+        @include py(0);
+        @include mx(10%);
+        @include my(5%);
+        background-color: $clr-white;
+        &:hover {
+          background: $clr-white * 0.8;
+        }
       }
     }
   }
 
-  .btn-enter {
-    width: $f-size-3 * 7;
-    height: $f-size-3 * 2;
-    border-radius: $f-size-3 * 1;
-    color: $clr-main-lv1;
-    font-size: $f-size-3;
-    line-height: $f-size-3 * 2;
-    font-weight: bold;
+  .btn-animal {
+    @include py(3px);
+    width: $f-size-2 * 5;
+    font-size: $f-size-2;
+    color: $clr-white;
+    border-color: $clr-white;
+    @media(min-width: $limit-w-pc) {
+      width: $f-size-3 * 5;
+      font-size: $f-size-3;
+    }
+    &:hover {
+      background: $clr-white * 0.5;
+    }
+    &-active {
+      color: $clr-selected;
+      border-color: $clr-selected;
+      &:hover {
+        background: $clr-selected * 0.5;
+      }
+    }
   }
 }
 .a1-animals {
