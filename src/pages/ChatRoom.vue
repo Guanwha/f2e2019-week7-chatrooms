c4ffa1870bc18b60eb87fa97ee81f3d54154ee65
Eric Fei <guanwha@gmail.com>
2019-09-01 16:42:57 +0800

    Created send-box layout of ChatRoom page
    1. implement send box
       - add button images
    
    2. modify _variables.scss
       - add color #968686

--- src/pages/ChatRoom.vue
+++ src/pages/ChatRoom.vue
@@ -1,17 +1,37 @@
 <template>
   <div class="full-wh flex-ccc">
     <div class="header flex-rlc">ChatRoom</div>
-    <div class="content"></div>
+    <div class="content flex-csbc">
+      <!-- message area -->
+      <div class="a1-messages">
+      </div>
+      <!-- send box -->
+      <div class="sendbox mt-auto flex-rcc">
+        <button class="img-btn"><img src="../assets/icon_emotion.svg"></button>
+        <button class="img-btn"><img src="../assets/icon_picture.svg"></button>
+        <button class="img-btn"><img src="../assets/icon_attachment.svg"></button>
+        <div class='line'></div>
+        <input class="form-control" type="text" v-model='message'>
+        <button class="img-btn"><img src="../assets/icon_send.svg"></button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ChatRoom',
+  data() {
+    return {
+      messages: {},
+      message: '',
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+@import '../styles/mixins';
 @import '../styles/variables';
 
 .header {
@@ -27,8 +47,42 @@ export default {
   width: 100%;
   height: calc(100% - 70px);
   background: $clr-main-lv2;
-  @media(min-width: $limit-w-pc) {
-    padding-bottom: 70px;
+  padding: 1rem * 0.5;
+  .sendbox {
+    width: 95%;
+    margin: 1rem * 0.5;
+    padding: 1rem * 0.5;
+    background: $clr-main-lv4;
+    border-radius: 1rem * 0.5;
+    .line {
+      width: 1px;
+      height: 2rem;
+      border: 0.5px solid white;
+      @include mx(1rem);
+    }
+    input {
+      @include mx(1rem * 0.5);
+      color: $clr-white;
+      background: $clr-main-lv5;
+      border-width: 0;
+      &:hover, &:focus {
+        border-color: $clr-white;
+        box-shadow: 0 0 0 0.1rem rgba(255,255,255,.5)
+      }
+    }
+  }
+}
+.img-btn {
+  padding: .5rem;
+  border-width: 0;
+  border-radius: .25rem;
+  background-color: transparent;
+  transition: background-color .15s ease-in-out;
+  &:hover {
+    background-color: #FFFFFF33;
+  }
+  &:disabled {
+    background-color: transparent;
   }
 }
 </style>
